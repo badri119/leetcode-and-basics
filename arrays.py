90de9100f318ce273b60659d55703dc9b4fb2dc7
badri119 <badrinathv98@gmail.com>
2023-10-08 00:42:02 -0700

    Move zeros to the end of the array

--- arrays.py
+++ arrays.py
@@ -169,7 +169,44 @@ class Solution:
          self.reverse(nums, k, len(nums) - 1)
          self.reverse(nums, 0, len(nums) - 1)
         
+#Leetcode 283. Move Zeroes
+#Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+# Note that you must do this in-place without making a copy of the array.
+# Example 1:
 
+# Input: nums = [0,1,0,3,12]
+# Output: [1,3,12,0,0]
+
+#BF Approach
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        nz = [] #Created a empty arrray non-zeros where I'll be storing all the non-zeros from the original array
+
+        for i in range(len(nums)): #looping through the array to find non-zeros
+            if nums[i] !=0:
+                nz.append(nums[i]) # appending it to the nz array
+        for i in range(len(nz)): #looping through the nz array
+            nums[i] = nz[i] #adding it back to the orginal array so first few spots will be filled up with non-zeros
+
+        for i in range(len(nz), len(nums)): #looping through the array starting from non zero position till the end of the nums array
+            nums[i] = 0 #adding zeros to the end
+            
+#Optimal Approach - Two pointers
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l = 0
+
+        for r in range(len(nums)):
+            if nums[r]:
+                nums[l], nums[r] = nums[r], nums[l]
+                l +=1
+        return nums
         
 
         
